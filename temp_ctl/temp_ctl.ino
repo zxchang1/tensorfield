29f1eb3260dc31b1a9b2a0ecab8de06d9b6dd19b
zxchang1 <33381191+zxchang1@users.noreply.github.com>
2019-05-14 18:22:39 +0800

    changed samples to 1000

--- temp_ctl/temp_ctl.ino
+++ temp_ctl/temp_ctl.ino
@@ -10,89 +10,86 @@ int H_Right = 5;
 int H_Nozzle = 6;
 
 void setup () {
-	Serial.begin(9600);
-	pinMode(H_Left, OUTPUT);
-	pinMode(H_Right, OUTPUT);
-	pinMode(H_Nozzle, OUTPUT);
+  Serial.begin(9600);
+  pinMode(H_Left, OUTPUT);
+  pinMode(H_Right, OUTPUT);
+  pinMode(H_Nozzle, OUTPUT);
 
 }
 
 void loop () {
-	
-	double T_BL = analogToTemp(averageRead(0)); //temp of back-left thermistor
-	double T_FL = analogToTemp(averageRead(1));
-	double T_BR = analogToTemp(averageRead(2));
-	double T_FR = analogToTemp(averageRead(3));
-	double T_NZ = analogToTemp(averageRead(4)); //temp of nozzle block
+  
+  double T_BL = analogToTemp(averageRead(0)); //temp of back-left thermistor
+  double T_FL = analogToTemp(averageRead(1));
+  double T_BR = analogToTemp(averageRead(2));
+  double T_FR = analogToTemp(averageRead(3));
+  double T_NZ = analogToTemp(averageRead(4)); //temp of nozzle block
 
-	String buf;
-	buf += F("\nT_BL = ");
-	buf += String(T_BL, 4);
-	
-	buf += F("\nT_FL = ");
-	buf += String(T_FL, 4);
+  String buf;
+  buf += F("\nT_BL = ");
+  buf += String(T_BL, 4);
+  
+  buf += F("\nT_FL = ");
+  buf += String(T_FL, 4);
 
-	buf += F("\nT_BR = ");
-	buf += String(T_BR, 4);
+  buf += F("\nT_BR = ");
+  buf += String(T_BR, 4);
 
-	buf += F("\nT_FR = ");
-	buf += String(T_FR, 4);
+  buf += F("\nT_FR = ");
+  buf += String(T_FR, 4);
 
-	buf += F("\nT_NZ = ");
-	buf += String(T_NZ, 4);
+  buf += F("\nT_NZ = ");
+  buf += String(T_NZ, 4);
 
-	Serial.println(buf);
+  Serial.println(buf);
 
-	if (max(T_BL, T_FL) >= setTemp) {
-		analogWrite(H_Left, 0);
-		Serial.println("H_Left OFF");
-	}
-	if (max(T_BL, T_FL) < setTemp) {
-		analogWrite(H_Left, 120);
-		Serial.println("H_Left ON");
-	}
+  if (max(T_BL, T_FL) >= setTemp) {
+    analogWrite(H_Left, 0);
+    Serial.println("H_Left OFF");
+  }
+  if (max(T_BL, T_FL) < setTemp) {
+    analogWrite(H_Left, 120);
+    Serial.println("H_Left ON");
+  }
 
-	if (max(T_BR, T_FR) >= setTemp) {
-		analogWrite(H_Right, 0);
-		Serial.println("H_Right OFF");
-	}
-	if (max(T_BR, T_FR) < setTemp) {
-		analogWrite(H_Right, 120);
-		Serial.println("H_Right ON");
-	}
+  if (max(T_BR, T_FR) >= setTemp) {
+    analogWrite(H_Right, 0);
+    Serial.println("H_Right OFF");
+  }
+  if (max(T_BR, T_FR) < setTemp) {
+    analogWrite(H_Right, 120);
+    Serial.println("H_Right ON");
+  }
 
-	if (T_NZ >= setTemp) {
-		analogWrite(H_Nozzle, 0);
-		Serial.println("H_Nozzle OFF");
-	}
-	if (T_NZ < setTemp) {
-		analogWrite(H_Nozzle, 120);
-		Serial.println("H_Nozzle ON");
-	}
+  if (T_NZ >= setTemp) {
+    analogWrite(H_Nozzle, 0);
+    Serial.println("H_Nozzle OFF");
+  }
+  if (T_NZ < setTemp) {
+    analogWrite(H_Nozzle, 120);
+    Serial.println("H_Nozzle ON");
+  }
 
-	delay(500);
+  delay(500);
 }
 
 double averageRead(int pin){
-	int samples = 10;
-	double accumulator = 0;
-	for (int i=1; i<=samples; i++){
-		accumulator += analogRead(pin);
-	}
+  int samples = 1000;
+  double accumulator = 0;
+  for (int i=1; i<=samples; i++){
+    accumulator += analogRead(pin);
+  }
         double average = accumulator/samples;
-	return average;	
+  return average; 
 }
 
 double analogToTemp(long pinLevel){
-	double voltage = pinLevel * (5.0 / 1024);
-	long beta = 3950;
-	long R0 = 100000;
-	long Rd = 1800;
-	double Rinf = R0 * exp(-1*beta/(25+273.15));
+  double voltage = pinLevel * (5.0 / 1024);
+  long beta = 3950;
+  long R0 = 100000;
+  long Rd = 1800;
+  double Rinf = R0 * exp(-1*beta/(25+273.15));
 
-	double T = beta/(log(Rd*((5/voltage)-1)/Rinf)) - 273.15;
-	return T;
+  double T = beta/(log(Rd*((5/voltage)-1)/Rinf)) - 273.15;
+  return T;
 }
-
-
-
